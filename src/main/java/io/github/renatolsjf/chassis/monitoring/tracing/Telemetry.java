0978258eeb2a7f6242a4be781ecb86f68e7244de
Renato Santos <renatolsjf@gmail.com>
2024-05-22 17:33:07 -0300

    Changed AppRegistry to enhance objects

--- src/main/java/io/github/renatolsjf/chassis/monitoring/tracing/Telemetry.java
+++ src/main/java/io/github/renatolsjf/chassis/monitoring/tracing/Telemetry.java
@@ -1,11 +1,24 @@
 package io.github.renatolsjf.chassis.monitoring.tracing;
 
-import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.exporter.logging.LoggingSpanExporter;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
+import io.opentelemetry.semconv.ResourceAttributes;
 
 public class Telemetry {
 
-    public OpenTelemetry openTelemetry() {
-        return null;
+    public Tracer tracer() {
+
+        Resource resource = Resource.getDefault().toBuilder()
+                .put(ResourceAttributes.SERVICE_NAME, "PLACEHOLDER").put(ResourceAttributes.SERVICE_VERSION, "0.0.1").build();
+
+        return SdkTracerProvider.builder()
+                .addSpanProcessor(SimpleSpanProcessor.create(LoggingSpanExporter.create()))
+                .setResource(resource)
+                .build()
+                .get("PLACEHOLDER");
     }
 
 }
--- src/main/java/io/github/renatolsjf/chassis/util/proxy/TracingEnhancer.java
+++ src/main/java/io/github/renatolsjf/chassis/util/proxy/TracingEnhancer.java
@@ -16,7 +16,7 @@ public class TracingEnhancer implements TypeEnhancer{
     public MethodInterceptor createInterceptor(Object delegate) {
         return (Object o, Method method, Object[] args, MethodProxy methodProxy) -> {
             System.out.println("ENHANCED: " + o.getClass().getSimpleName() + " - " + method.getName());
-            Tracer tracer = new Telemetry().openTelemetry().getTracer(o.getClass().getSimpleName()); // TODO this needs to be in the context so the tracer is already initialized
+            Tracer tracer = new Telemetry().tracer(); // TODO this needs to be in the context so the tracer is already initialized
             Span span = tracer.spanBuilder(method.getName()).startSpan(); //TODO span annotation to configure
             try (Scope scope = span.makeCurrent()) {
                 return method.invoke(delegate != null ? delegate : o, args);
